00c412597cf1e7e0091163af79d6d832da3f1bc3
schattopadhyaya <Schattopadhyaya@nextgen.com>
2020-05-10 23:09:06 +0530

    Updated project id of Project 1

--- Project 1/provider.tf
+++ Project 1/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = file("../learn-terraform.json")
-  project     = "bold-gadget-222718"
+  project     = "project-id"
   region      = "asia-south1"
   zone        = "asia-south1-b"
 }
